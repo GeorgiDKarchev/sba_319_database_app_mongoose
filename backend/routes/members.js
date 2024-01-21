423072f6d48bacc3445dc5a7ccf84fc4449a0194
[Georgi Karchev] <georgikarcev@gmail.com>
2024-01-21 00:40:42 -0500

    Adding PUT route and updating member data

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -1,14 +1,14 @@
-import{Router} from 'express';
+import { Router } from 'express';
 import Member from '../models/members.js';
 
 
-const router = new Router ();
+const router = new Router();
 
 /**
  * GET - return all members
  */
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     const members = await Member.find({});
     res.status(200).json(members);
 });
@@ -17,12 +17,12 @@ router.get('/', async (req,res)=>{
  * GET /:id 
  * return member by id
  */
- router.get('/:id', async (req, res) => {
-    const member =  await Member.findById(req.params.id);
+router.get('/:id', async (req, res) => {
+    const member = await Member.findById(req.params.id);
 
-    if(!member) return res.status(404).json({msg:"Member id not found"});
+    if (!member) return res.status(404).json({ msg: "Member id not found" });
     else res.json(member);
- });
+});
 // router.get("/:id", async(req, res)=>{
 //     const member = await Member.findById(req.params.id);
 //     if (!member) res.send("Not found").status(404);
@@ -34,10 +34,38 @@ router.get('/', async (req,res)=>{
 /**
  * POST - create a new member
  */
-router.post('/', async (req, res)=>{
+router.post('/', async (req, res) => {
     const member = await Member.create(req.body);
     res.status(201).json(member);
 });
+
+
+
+/**
+ * PUT /:id
+ * Update Members data 
+ */
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { body } = req;
+        const updatedMember = await Member.findByIdAndUpdate(id, body, { new: true });
+        res.json(updatedMember);
+    } catch (error) {
+        console.log(error);
+        res.json({ msg: 'Member not found' });
+    }
+
+
+});
+
+
+
+
+/**
+ * DELETE /:id
+ */
+
 
 
 
